3492337e52c2acc73b8a147e317aacbf432c3ae3
User <106451224+0xtanmaye@users.noreply.github.com>
2023-11-17 07:09:36 +0530

    Check the condition immediately after accepting sourcef

--- bash_scripting/file_directory_copy.sh
+++ bash_scripting/file_directory_copy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 read -p "Enter the source filename: " sourcef
+if ! [ -e $sourcef ]; then
+	echo "$sourcef doesn't exist!"
+	exit 1
+fi
 read -p "Enter the destination filename: " destinationf
 
 if [ -f $sourcef ]
@@ -10,7 +14,4 @@ elif [ -d $sourcef ]
 then
 	cp -rv $sourcef $destinationf
 	echo "$sourcef copied as $destinationf successfully"
-elif ! [ -e $sourcef ]
-then
-	echo "$sourcef doesn't exist"
 fi
