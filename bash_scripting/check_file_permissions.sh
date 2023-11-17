75cf87bb43de7466022bd3050d49994920ff88c2
User <106451224+0xtanmaye@users.noreply.github.com>
2023-11-17 08:44:27 +0530

    Update check_file_permissions.sh with better messages & file exists check

--- bash_scripting/check_file_permissions.sh
+++ bash_scripting/check_file_permissions.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 read -p "Enter name: " input_name
+# OR
+# input_name=$1
 
+if [ ! -e $input_name ]
+then
+	echo "$input_name doesn't exist"
+	exit 1
+fi
 if [ -r $input_name ]
 then
-	echo "$input_name has read permission"
+	echo "$input_name is readable"
 fi
 if [ -w $input_name ]
 then
-	echo "$input_name has write permission"
+	echo "$input_name is writeable"
 fi
 if [ -x $input_name ]
 then
-	echo "$input_name has execute permission"
+	echo "$input_name is executable"
 fi
